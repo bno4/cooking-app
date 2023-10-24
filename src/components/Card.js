db10e06d0348a8d320c1a7160b86650bf760239b
bno4 <bnduchemin@gmail.com>
2023-10-24 17:35:19 +0200

    add style on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ meal }) => {
       <h3>{meal.strMeal}</h3>
       <p>Origin : {meal.strArea}</p>
       <img src={meal.strMealThumb} alt={"photo de " + meal.strMeal} />
-      <p>{meal.strInstructions}</p>
+      <p className="description">{meal.strInstructions}</p>
     </div>
   );
 };
